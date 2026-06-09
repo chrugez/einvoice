a5eda718cd0f7d9c647eef414394af77c2cc76f7
chrugez <thanhcuis99@gmail.com>
2026-06-09 23:44:57 +0700

    feat: setup husky

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import {PORT} from '@common/constants/common.constants'
+import { PORT } from '@common/constants/common.constants'
 
 @Injectable()
 export class AppService {
