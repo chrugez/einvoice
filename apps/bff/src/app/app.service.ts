44167b76ee61afca852f5a2dee0c7fa61ff8e7a7
chrugez <thanhcuis99@gmail.com>
2026-06-09 23:32:24 +0700

    feat: generate libs

--- apps/bff/src/app/app.service.ts
+++ apps/bff/src/app/app.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import {PORT} from '@common/constants/common.constants'
 
 @Injectable()
 export class AppService {
   getData(): { message: string } {
+    console.log(PORT)
     return { message: 'Hello API' };
   }
 }
